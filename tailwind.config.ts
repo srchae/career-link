c8152ab358ca6e5c29986a598172828230d97d74
srchae <srchae@jdi-global.com>
2025-01-10 19:00:38 +0900

    🏷️ Feat: config 파일 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,30 @@ export default {
   ],
   theme: {
     extend: {
+      variants: {
+        scrollbars: ["hover", "responsive"],
+      },
+      fontFamily: {
+        default: ["WantedSans", "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }: any) {
+      addUtilities({
+        ".scrollbar-hide": {
+          /* 스크롤 동작 유지하면서 숨기기 */
+          "-ms-overflow-style": "none", // IE, Edge
+          "scrollbar-width": "none", // Firefox
+          "&::-webkit-scrollbar": {
+            display: "none", // Chrome, Safari
+          },
+        },
+      });
+    },
+  ],
 } satisfies Config;
