ff639d925c3f64985b8971faee24be2e72322b4d
srchae <srchae@jdi-global.com>
2025-01-16 18:45:39 +0900

    💄 Style: 아이콘 추가 및 스타일 수정

--- src/components/inputs/Searchbar.tsx
+++ src/components/inputs/Searchbar.tsx
@@ -6,7 +6,7 @@ export const Searchbar = () => {
       <input
         type="text"
         placeholder="관심있는 내용을 검색해보세요!"
-        className="w-full pl-4 pr-10 py-2 border rounded-full shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+        className="w-full pl-4 pr-10 py-2 border rounded-full shadow-sm focus:outline-[#aaa] focus:outline-dashed focus:outline-1 focus:outline-none"
       />
       <button
         type="submit"
